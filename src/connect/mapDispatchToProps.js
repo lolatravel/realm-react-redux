8bb90c934c3a7074cd5d8272249bcf448d6071f4
Venkat Mithun <41567130+venkatmithunk@users.noreply.github.com>
2019-07-30 12:07:05 +0530

    Update mapDispatchToProps.js

--- src/connect/mapDispatchToProps.js
+++ src/connect/mapDispatchToProps.js
@@ -1,4 +1,4 @@
-import bindActionCreators from 'redux';
+import { bindActionCreators } from 'redux';
 import { wrapMapToPropsConstant, wrapMapToPropsFunc } from './wrapMapToProps';
 
 export function whenMapDispatchToPropsIsFunction(mapDispatchToProps) {
